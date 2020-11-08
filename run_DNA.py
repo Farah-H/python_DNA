c2d20c1b0d8504f1e250ea835610efc7da5c9ac3
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-08 20:48:43 +0000

    minor edits

--- run_DNA.py
+++ run_DNA.py
@@ -2,9 +2,10 @@ from DNA_class import DNA
 
 
 while True:
-    dna_string = input('Please insert your DNA string').upper()
+    dna_string = input('Please insert your DNA string. Note: your string cannot be longer than 1000 characters').upper()
     if len(dna_string) > 1000:
-        continue    
+        continue   
     else: 
         dna_instance = DNA()
         dna_instance.count_DNA(dna_string)
+        break 
