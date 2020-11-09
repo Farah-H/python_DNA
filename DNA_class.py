ec0b932204363de94ef82a382d81975a9a187616
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-09 08:23:22 +0000

    added pseudo code, edited README

--- DNA_class.py
+++ DNA_class.py
@@ -1,16 +1,19 @@
 class DNA:
     
+    # Initialising the class, defining a,c,g,t
     def __init__(self):
         self.a = 'A'
         self.c = 'C'
         self.g = 'G'
         self.t = 'T'
 
+    # this function counts the number of times each of the symbols appears in a string
     def count_DNA(self, string_input):
         count_a = string_input.count(self.a)
         count_c = string_input.count(self.c)
         count_g = string_input.count(self.g)
         count_t = string_input.count(self.t)
 
+        # returning an f-string showing the number of times each symbol was counted
         print(f'{self.a} {self.c} {self.g} {self.t}')
         print(f'{count_a} {count_c} {count_g} {count_t}')
--- run_DNA.py
+++ run_DNA.py
@@ -1,11 +1,15 @@
+# importing DNA class
 from DNA_class import DNA
 
 
+# If the string is longer than 1000 characters
+# the user will be prompted to enter a string again
 while True:
     dna_string = input('Please insert your DNA string. Note: your string cannot be longer than 1000 characters').upper()
     if len(dna_string) > 1000:
         continue   
     else: 
+        # instantiating the class so we can use the method
         dna_instance = DNA()
         dna_instance.count_DNA(dna_string)
         break 
